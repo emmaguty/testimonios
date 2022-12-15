507a653fcf5fe44991e8b84e653ea2ed1fecbf37
emmanuelguty <manuel15901@gmail.com>
2022-12-15 15:46:27 -0600

    Se creo un nuevo componente

---
+++ src/components/Testimonio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Testimonio() {
+  return(
+    <div className='contenedor-testimonio'>
+      <img 
+      className='imagen-testimonio'
+      src={require('../images/testimonio-1.jpg')}
+      alt='Primer testimonio'
+      />
+    </div>
+  );
+}
