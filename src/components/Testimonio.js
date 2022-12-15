6cdc78265229ca6194c9a53aa5bc91e9ae7aff9a
emmanuelguty <manuel15901@gmail.com>
2022-12-15 16:17:09 -0600

    Se desarrollo el componente testimonio pero aun falta los props

--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
+//Componente testimonio
 function Testimonio() {
   return(
     <div className='contenedor-testimonio'>
+      {/* /* Imagen */}
       <img 
       className='imagen-testimonio'
       src={require('../images/testimonio-1.jpg')}
       alt='Primer testimonio'
       />
+      {/* Texto del testimonio: nombre, cargo y breve desc. */}
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>Elon Musk en EUA</p>
+        <p className='cargo-testimonio'>CEO de Tesla</p>
+        <p className='texto-testimonio'>Elon Reeve Musk, conocido como Elon Musk, es un empresario, inversionista y magnate sudafricano, canadiense y estadounidense</p>
+      </div>
     </div>
   );
 }
+
+export default Testimonio;
+
